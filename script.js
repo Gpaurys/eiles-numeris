240ee1ecc6691081da4378943dd78b4a42ee382a
Giedrius <paurys79@gmail.com>
2021-06-03 12:06:12 +0300

    Įkėliau eilės numerį

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = prompt("Koks tavo vardas?");
+alert("Naujas dalyvis: " + name);
+
+var names = ["JonSnow", "Cersei Lannister", "Daenerys Targaryen",
+"Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+var number = prompt("Koks tai skaičius?");
+console.log("Dalyvio vardas: " + names[number] + ". ");
